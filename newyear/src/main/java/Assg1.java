b77d1c048980830ad636b767b95ef673c7ce96f5
Sai Sumanth <Sai Sumanth@Sumanth-PC>
2020-02-08 23:21:09 +0530

    changed

--- newyear/src/main/java/Assg1.java
+++ newyear/src/main/java/Assg1.java
@@ -1,7 +1,6 @@
-
-
-public class Assg1 {
-public static void main(String[],args)
+package main.java
+public class Assg1{
+public static void main(String[] args)
 {
 	Ngif v=new Ngif();
 	v.inputSweets();
